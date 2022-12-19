a2d414b7de2ddf5027465bd8f3daaa69c081b322
Sisay Mandefro <sisimandela19@gmail.com>
2022-12-19 10:25:51 +0300

    updated script

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ let loginForm = document.querySelector('.login-form-container');
 document.querySelector('#login-btn').onclick = () =>{
   loginForm.classList.toggle('active');
 }
-
 document.querySelector('#close-login-btn').onclick = () =>{
   loginForm.classList.remove('active');
 }
